78a0685b10e373e6e3ab6e3d2daf90ed0f60c790
kanth5 <mrajk5@gmail.com>
2024-01-10 15:42:00 +0530

    Define Entity, Create Repository and perform CRUD operations

---
+++ tennis-player-jpa-demo/src/test/java/com/springdatajpa/tennisplayer/TennisPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.springdata.tennisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
